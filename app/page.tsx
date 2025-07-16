984423d7287ad6313fbb297d39982983ef533923
Joe Pigott <joe.pigs@protonmail.com>
2025-07-15 20:08:07 -0600

    fixed scroll locking when switching to medium viewport while drawer is open

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import HighlightText from "./ui/highlight";
+import { montserrat } from "./ui/fonts";
 
 export default function Home() {
     return (
-        <div className="flex flex-col">
-            <div className="h-[100svh] md:-mt-30 mb-5 flex justify-center items-center">
+        <div className={`${montserrat.className} antialiased flex flex-col`}>
+            <div className="h-[100svh] md:-mt-30 flex justify-center items-center">
                 <h1 className="text-center">Mission critical <HighlightText>software</HighlightText>, built with the precision of a  <HighlightText>physicist</HighlightText>.</h1>
             </div>
         </div>
--- app/ui/mobile_nav.tsx
+++ app/ui/mobile_nav.tsx
@@ -1,4 +1,4 @@
-'use client';
+'use client'
 
 import { useState, useEffect } from "react";
 import { space_grotesk } from "./fonts";
@@ -10,22 +10,19 @@ export function MobileNavBar({ displayType }: { displayType: string }) {
 
     useEffect(() => {
         if (isVisible) {
-            document.body.style.overflow = "hidden";
-        } else {
-            document.body.style.overflow = "";
+            document.body.classList.add("mobile-scroll-lock");
         }
-
-        return () => {
-            document.body.style.overflow = "";
-        };
-    });
+        else {
+            document.body.classList.remove("mobile-scroll-lock");
+        }
+    }, [isVisible]);
 
     return (
-        <div className={`${displayType} fixed top-0`}>
-            <button onClick={() => { setVisible(!isVisible); console.log(isVisible); } } className="z-50 rounded-full border-1 border-lowgray text-center p-3 m-4 bg-background/60 backdrop-blur">
+        <div className={`${displayType} relative`}>
+            <button onClick={() => { setVisible(!isVisible); console.log(isVisible); } } className="fixed z-10 rounded-full border-1 border-lowgray text-center p-3 m-4 bg-background/60 backdrop-blur">
                 { isVisible ? <XIcon /> : <Menu /> }
             </button>
-            <nav className={`${space_grotesk.className} antialiased flex flex-col h-[100dvh] w-[100vw] bg-background/80 backdrop-blur p-4 text-4xl gap-3 transition-transform duration-200 ease-out z-50 ${isVisible ? 'translate-x-0' : '-translate-x-full'}`}>
+            <nav className={`${space_grotesk.className} antialiased fixed pt-[5.2rem] flex flex-col h-[100dvh] w-[100vw] bg-background/80 backdrop-blur p-4 text-4xl gap-3 transition-transform duration-200 ease-out z-9 ${isVisible ? 'translate-x-0' : '-translate-x-full'}`}>
                 <Link href="/">Home </Link>
                 <Link href="/projects">Projects</Link>
                 <Link href="/about">About</Link>
